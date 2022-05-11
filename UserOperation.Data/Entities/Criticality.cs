d018b7e81069cc53b1e8afdc0c3570539a19a5bf
NESS\V3790463 <andrei.ciocan@ness.com>
2022-05-11 18:32:51 +0300

    Add Entities

---
+++ UserOperation.Data/Entities/Criticality.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperation.Data.Entities
+{
+    public class Criticality
+    {
+        public int CriticalityID { get; set; }
+        public string CriticalityName { get; set; }
+
+    }
+}
---
+++ UserOperation.Data/Entities/Stability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperation.Data.Entities
+{
+    public class Stability
+    {
+        public int StabilityId { get; set; }
+        public virtual Employee Employee { get; set; }
+        public string StabilityMonth { get; set; }
+        public int LeavingYear { get; set; }
+        public virtual StabilityLevel StabilityLevel { get; set; }
+        public virtual Criticality Criticality { get; set; }
+
+    }
+}
