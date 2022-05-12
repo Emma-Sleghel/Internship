e05a28a0f58492756fff1adcf09b9ec808b33db8
NESS\V3790473 <cosmin.martin@ness.com>
2022-05-12 15:52:26 +0300

    Add LeaveService

--- UserOperation.Services/Dtos/LeaveDto.cs
+++ UserOperation.Services/Dtos/LeaveDto.cs
@@ -8,5 +8,13 @@ namespace UserOperation.Services.Dtos
 {
     public class LeaveDto
     {
+        public int LeaveID { get; set; }
+        public int EmployeeId { get; set; }
+        public int LeaveMonth { get; set; }
+        public int LeaveYear { get; set; }
+        public int ActiveHC { get; set; }
+        public int PrimaryReasonId { get; set; }
+        public int SecondaryReasonId { get; set; }
+
     }
 }
---
+++ UserOperation.Services/Services/ILeaveService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperation.Services.Dtos;
+
+namespace UserOperation.Services.Services
+{
+    public interface ILeaveService
+    {
+        LeaveDto GetLeaveById(int id);
+        ICollection<LeaveDto> GetAllLeaves();
+        void CreateLeave(LeaveDto leave);
+        void UpdateLeave(LeaveDto leave);
+        bool DeleteLeave(int id);
+    }
+}
