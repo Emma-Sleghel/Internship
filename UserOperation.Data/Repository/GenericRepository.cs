961258a6cb999a80a139c2acf42eecbd0527f5c3
NESS\V3790473 <cosmin.martin@ness.com>
2022-05-12 08:27:51 +0300

    Adding GenericRepository

---
+++ UserOperation.Data/Repository/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserOperation.Data.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        private DbSet<T> entities;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            entities = _context.Set<T>();
+        }
+        public T GetById(int id)
+        {
+            return entities.Find(id);
+        }
+
+        public ICollection<T> GetAll()
+        {
+            return entities.ToList();
+        }
+
+        public void Create(T entity)
+        {
+            entities.Add(entity);
+            Save();
+        }
+
+        public void Update(T entity)
+        {
+            entities.Update(entity);
+            Save();
+        }
+
+        public void Delete(T entity)
+        {
+            entities.Remove(entity);
+            Save();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        
+        public IEnumerable<T> Query(Func<T, bool> expression)
+        {
+            return entities.Where(expression);
+        }
+    }
+}
