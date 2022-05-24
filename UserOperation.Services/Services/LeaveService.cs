6e728aefdcb29d8a7b7e5a069e3a857ee33209f9
NESS\V3790480 <Elena.Axinte@ness.com>
2022-05-24 13:11:19 +0300

    Add-EditLeave

--- UserOperation.Services/Services/LeaveService.cs
+++ UserOperation.Services/Services/LeaveService.cs
@@ -32,29 +32,35 @@ namespace UserOperation.Services.Services
 
         public LeaveDto GetLeaveById(int id)
         {
-            var leave = _mapper.Map<LeaveDto>(_leaveRepository.GetById(id));
+            // var leave = _mapper.Map<LeaveDto>(_leaveRepository.GetById(id));
+            var leave = _mapper.Map<LeaveDto>(_leaveRepository.Query().Where(x => x.LeaveID == id)
+                .Include(x => x.Employee).ThenInclude(x => x.Level)
+                .Include(x => x.Employee).ThenInclude(x => x.Position)
+                .Include(x => x.Employee).ThenInclude(x => x.Projects)
+                .Include(x=> x.PrimaryReason)
+                .Include(x=>x.SecondaryReason).FirstOrDefault());
             return leave;
         }
 
         public ICollection<LeaveDto> GetAllLeaves()
         {
-            var leaves = _mapper.Map<List<LeaveDto>>(_leaveRepository.Query().Include(x=>x.Employee).ThenInclude(x=>x.Projects)
-                .Include(x => x.Employee).ThenInclude(x=>x.Level)
-                .Include(x=>x.Employee).ThenInclude(x=>x.Position)
-                .Include(x=>x.PrimaryReason)
-                .Include(x=>x.SecondaryReason)
+            var leaves = _mapper.Map<List<LeaveDto>>(_leaveRepository.Query().Include(x => x.Employee).ThenInclude(x => x.Projects)
+                .Include(x => x.Employee).ThenInclude(x => x.Level)
+                .Include(x => x.Employee).ThenInclude(x => x.Position)
+                .Include(x => x.PrimaryReason)
+                .Include(x => x.SecondaryReason)
                 );
-               
+
             return leaves;
         }
         public int? CreateLeave(LeaveDto leave)
         {
-            var leaveEntity = _leaveRepository.Query().Include(e=>e.Employee).Where(l=>l.Employee.EmployeeID == leave.Employee.EmployeeId)
+            var leaveEntity = _leaveRepository.Query().Include(e => e.Employee).Where(l => l.Employee.EmployeeID == leave.Employee.EmployeeId)
                 .FirstOrDefault();
 
             if (leaveEntity != null)
                 return null;
-            
+
             var employee = _employeeRepository.GetById(leave.Employee.EmployeeId);
             if (employee == null)
             {
@@ -68,26 +74,29 @@ namespace UserOperation.Services.Services
             leaveMap.Employee = employee;
             leaveMap.PrimaryReason = primaryReason;
             leaveMap.SecondaryReason = secondaryReason;
-           
+
             _leaveRepository.Create(leaveMap);
             return leaveMap.LeaveID;
         }
 
         public LeaveDto UpdateLeave(LeaveDto leave)
         {
-            var leaveEntity = _leaveRepository.Query(l => l.Employee.EmployeeID == leave.EmployeeId)
-                .FirstOrDefault();
-            if (leaveEntity == null)
-                return null;
-
             var dbLeave = _leaveRepository.GetById(leave.LeaveID);
-            var employee = _employeeRepository.GetById(leave.EmployeeId);
-            var primaryReason = _reasonRepository.GetById(leave.PrimaryReasonId);
-            var secondaryReason = _reasonRepository.GetById(leave.SecondaryReasonId);
+            var employee = _employeeRepository.GetById(leave.Employee.EmployeeId);
+            var primaryReason = _reasonRepository.GetById(leave.PrimaryReason.ReasonId);
+            var secondaryReason = _reasonRepository.GetById(leave.SecondaryReason.ReasonId);
             dbLeave.LeaveMonth = leave.LeaveMonth;
             dbLeave.LeaveYear = leave.LeaveYear;
             dbLeave.ActiveHC = leave.ActiveHC;
-            
+            if (employee == null)
+            {
+                _employeeService.CreateEmployee(leave.Employee);
+            }
+            else
+            {
+                _employeeService.UpdateEmployee(leave.Employee);
+            }
+            employee = _employeeRepository.GetById(leave.Employee.EmployeeId);
             dbLeave.Employee = employee;
             dbLeave.PrimaryReason = primaryReason;
             dbLeave.SecondaryReason = secondaryReason;
@@ -100,13 +109,13 @@ namespace UserOperation.Services.Services
 
         public bool DeleteLeave(int id)
         {
-            
+
             Leave leave = _leaveRepository.GetById(id);
-            if(leave == null)
+            if (leave == null)
             {
                 return false;
             }
-         
+
             _leaveRepository.Delete(leave);
 
             return true;
